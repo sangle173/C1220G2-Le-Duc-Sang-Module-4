cc7cb02ec97bc40bc6185d1a9a12f9622926edd6
sangle173 <76678575+sangle173@users.noreply.github.com>
2021-05-05 00:12:18 +0700

    update

--- lesson_3_views_thymeleaf/practises/src/main/java/com/codegym/service/CustomerService.java
+++ lesson_3_views_thymeleaf/practises/src/main/java/com/codegym/service/CustomerService.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class CustomerService implements ICustomerService{
+public class CustomerService implements ICustomerService {
     private static final Map<Integer, Customer> customers;
 
     static {

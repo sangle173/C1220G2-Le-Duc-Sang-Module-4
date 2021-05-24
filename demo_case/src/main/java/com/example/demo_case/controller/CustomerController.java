b1e82d7ac3e48ff54abb948d1deebefefed4a0e8
sangle173 <76678575+sangle173@users.noreply.github.com>
2021-05-24 11:38:02 +0700

    update

--- demo_case/src/main/java/com/example/demo_case/controller/CustomerController.java
+++ demo_case/src/main/java/com/example/demo_case/controller/CustomerController.java
@@ -5,10 +5,8 @@ import com.example.demo_case.service.customer.CustomerService;
 import com.example.demo_case.service.customer.TypeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/customers")
@@ -36,4 +34,12 @@ public class CustomerController {
 
         return "redirect:/customers";
     }
+
+
+    @GetMapping(value = "/delete/{id}")
+    public String view(@PathVariable("id") Integer id) {
+        customerService.deleteById(id);
+        return "redirect:/customers";
+    }
+
 }
--- demo_case/src/main/java/com/example/demo_case/controller/EmployeeController.java
+++ demo_case/src/main/java/com/example/demo_case/controller/EmployeeController.java
@@ -6,7 +6,6 @@ import com.example.demo_case.service.employee.EducationDegreeService;
 import com.example.demo_case.service.employee.EmployeeService;
 import com.example.demo_case.service.employee.PositionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -58,7 +57,7 @@ public class EmployeeController {
     }
 
     @PostMapping("/edit")
-    public String editEmployee(Model model, @RequestParam("id") Integer id, @ModelAttribute Employee employee) {
+    public String editEmployee(@ModelAttribute Employee employee) {
         employeeService.save(employee);
         return "redirect:/employees";
     }
